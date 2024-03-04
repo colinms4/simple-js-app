7c7549e4f97e6dd680bcd67fcff84c651c1d19a5
colinms4 <zorbology@gmail.com>
2024-03-03 22:42:24 -0800

    added IIFE, forEach loop, getAll and Add code to display pokemon repo

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-let pokemonList = [
+// pokemon app
+// start of IIFE
+let pokemonRepository = (function() {
+    let pokemonList = [
     {
         name: 'Charmander',
         height: 0.6,
@@ -18,14 +21,31 @@ let pokemonList = [
     }
 ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 0.6) {
-        document.write("Name: " + pokemonList[i].name + " ( Height: " + pokemonList[i].height + " ) - Wow, that's big! ");
-    } else if (pokemonList[i].height <= 0.6) {
-        document.write("Name: " + pokemonList[i].name + " ( Height: " + pokemonList[i].height + " ) ");
-    }
-    document.write('<br>');
+// function to add pokemon to the array
+function add(pokemon) {
+    pokemonList.push(pokemon);
 }
 
+//function to get all pokemon in the array
+function getAll() {
+    return pokemonList;
+}
 
+return {
+    add:add,
+    getAll:getAll
+};
+})();
 
+// Get the list of Pokemon
+let allPokemon = pokemonRepository.getAll();
+
+// Iterate through each Pokemon and display its details
+allPokemon.forEach(function(pokemon) {
+  document.write(`<p>Name: ${pokemon.name}, Height: ${pokemon.height}, Type: ${pokemon.type.join(', ')}</p>`);
+});
+
+// adding a pokemon to the array
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({name: 'Pikachu', height: 0.2, type: ['Electric'] });
+console.log(pokemonRepository.getAll());
